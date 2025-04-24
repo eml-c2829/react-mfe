046dd093dac3fbf2677b47e215471fd160ec5be3
Van Dang <v.thai@eml.com.au>
2025-04-24 11:32:05 +0700

    refactor: move footer content to a separate Footer component

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -3,6 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Footer() {
+  return (
+    <>
+      <p>
+        This is footer component
+      </p>
+      <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+      </p>
+      <p>
+        Van Thai
+      </p>
+    </>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -25,12 +41,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>
-        Van Thai
-      </p>
+      <Footer />
     </>
   )
 }
