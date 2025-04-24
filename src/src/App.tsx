ea973f3dbd5b84a52778cbf7377accbb4f1635ef
Van Dang <v.thai@eml.com.au>
2025-04-24 14:17:52 +0700

    refactor: enhance Footer component with props and status management

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,47 +1,111 @@
-import { useState } from 'react'
+import { memo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Footer() {
+type FooterProps = {
+  author: AuthorDetail
+}
+
+type AuthorDetail = {
+  name: string
+  age: number,
+  favoriteLanguages: string[]
+}
+
+const Footer = memo(function Footer(props: FooterProps) {
+  const textColor = 'blue';
+
+  const [status, setStatues] = useState('active');
+
+  console.log('Footer component is re-rendered');
+
   return (
     <>
       <p>
-        This is footer component
+        This is footer component. Status is {status}
+      </p>
+      <p>
+        <button onClick={() => setStatues('inactive')}>Set Inactive</button>
+        <button onClick={() => setStatues('active')}>Set Active</button>
       </p>
       <p className="read-the-docs">
       Click on the Vite and React logos to learn more
       </p>
+      <p style={{ color: textColor }}>
+        Author: {props.author.name},
+        Age: {props.author.age},
+      </p>
       <p>
-        Van Thai
+      Favorite Languages: 
+      <ul>
+        {props.author.favoriteLanguages.map((language, index) => (
+          <li key={index}>{index}. {language}</li>
+        ))}
+      </ul>
       </p>
     </>
   )
+});
+
+const Logo = memo(function Logo() {
+  console.log('Logo component is re-rendered');
+
+  return (
+    <div className="flex items-center justify-center bg-blue-900">
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
+  );
+});
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  console.log('Counter component is re-rendered');
+
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [status, setStatues] = useState('active');
+
+
+  const authorDetail: AuthorDetail = {
+    name: 'Thai Van',
+    age: 20,
+    favoriteLanguages: ['JavaScript', 'TypeScript', 'Python'],
+  };
+
+  console.log('App component is re-rendered');
+
 
   return (
     <>
-      <div className="flex items-center justify-center bg-blue-900">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <Logo />
       <h1 className="text-4xl font-bold text-blue-500">Welcome to Tailwind CSS!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <Footer />
+      <p>
+        App status is {status}
+        <button onClick={() => setStatues('inactive')}>Set Inactive</button>
+        <button onClick={() => setStatues('active')}>Set Active</button>
+      </p>
+      <Counter />
+      <Footer 
+        author={authorDetail} 
+      />
     </>
   )
 }
